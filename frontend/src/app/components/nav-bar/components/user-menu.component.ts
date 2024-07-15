2200f89e51ba0e435763ae6f905eb38a8dec77ec
Daynya <daynya_l_quigley@progressive.com>
2024-07-15 12:33:50 -0400

    Before lunch day one

---
+++ frontend/src/app/components/nav-bar/components/user-menu.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-menu',
+  standalone: true,
+  imports: [],
+  template: `
+      <a class="btn">Click Me for Tacos</a>
+  `,
+  styles: ``
+})
+export class UserMenuComponent {
+
+}
